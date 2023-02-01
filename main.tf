625ae99d9db484b6100e0b95a67c54908ac8f9c9
joestack <joern@joern-online.de>
2023-02-01 14:51:24 +0100

    fix 2

--- main.tf
+++ main.tf
@@ -18,11 +18,6 @@ provider "aws" {
   region = var.aws_region
   access_key = data.vault_aws_access_credentials.creds.access_key
   secret_key = data.vault_aws_access_credentials.creds.secret_key
-
-  default_tags {
-    environment = var.name
-    owner       = joestack
-  }
 }
 
 
