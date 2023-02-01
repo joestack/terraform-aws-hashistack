f152a123e1c74ec2fce2c632e48ed100e39b4c30
joestack <joern@joern-online.de>
2023-02-01 14:47:53 +0100

    fix 1

--- main.tf
+++ main.tf
@@ -9,6 +9,11 @@ provider "vault" {
   }
 }
 
+data "vault_aws_access_credentials" "creds" {
+  backend = "aws"
+  role    = "deploy"
+}
+
 provider "aws" {
   region = var.aws_region
   access_key = data.vault_aws_access_credentials.creds.access_key
